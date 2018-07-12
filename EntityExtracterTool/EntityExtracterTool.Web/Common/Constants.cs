d2db093ba34e79d91db97a09c4f09cc90626e10b
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-12 11:50:15 +0300

    Added ShowEntityInformation view and updated HomeController

--- EntityExtracterTool/EntityExtracterTool.Web/Common/Constants.cs
+++ EntityExtracterTool/EntityExtracterTool.Web/Common/Constants.cs
@@ -5,8 +5,8 @@
         public const string ProjectName = "EntityExtracterTool.Sitefinity.";
         public const string SitefinityAssemblyName = "Telerik.Sitefinity";
 
-        public const string VersionTen = "10";
-        public const string VersionEleven = "11";
+        public const string PreviousVersion = "10";
+        public const string CurrentVersion = "11";
 
         public const string BinFolderPath = "\\bin\\";
         public const string DllType = "*.dll";
--- EntityExtracterTool/EntityExtracterTool.Web/Controllers/HomeController.cs
+++ EntityExtracterTool/EntityExtracterTool.Web/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+
+using Bytes2you.Validation;
+
+using EntityExtracterTool.Web.Common;
+using EntityExtracterTool.Web.Models;
+using EntityExtracterTool.Web.Services.Contracts;
 
 namespace EntityExtracterTool.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEntityComparer entityComparer;
+
+        public HomeController(IEntityComparer entityComparer)
+        {
+            Guard.WhenArgument(entityComparer, "Entity Comparer").IsNull().Throw();
+
+            this.entityComparer = entityComparer;
+        }
+
         public ActionResult Index()
         {
-            return View();
-        }     
+            return this.View();
+        }
+
+        public ActionResult ShowEntityInformation()
+        {
+            var entityHolder = new EntityHolder()
+            {
+                AddedEntities = new List<Entity>(),
+                UpdatedEntities = new List<Entity>(),
+                RemovedEntities = new List<Entity>()
+            };
+
+            this.entityComparer.CompareEntities(entityHolder, Constants.PreviousVersion, Constants.CurrentVersion);
+
+            return this.View(entityHolder);
+        }
     }
 }
